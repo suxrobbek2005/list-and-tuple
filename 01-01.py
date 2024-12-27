847c7e55af0cd22f098e63b274a6968669af5a23
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2024-12-27 09:53:46 -0500

    Done

---
+++ 01-01.py
@@ -0,0 +1,7 @@
+# O'zgaruvchi yaratib oldik
+x = 5
+b = x # X o'zgaruvchining qiymati B o'zgaruvchiga taminlandi
+
+print("O'zgaruvchilarni Identifierlari:")
+print (id(x)) # X o'zgaruvchining identiferi chiqarildi 
+print (id(b)) # B o'zgaruvchining identiferi chiqarildi
