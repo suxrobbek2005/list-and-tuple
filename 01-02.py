8713dcefa9f1e80b8821a1a4267974d9c475f55d
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2024-12-27 10:04:52 -0500

    Done

---
+++ 01-02.py
@@ -0,0 +1,5 @@
+# O'zgaruvchilar yaratib olindi 
+username, name, ege = "Yaxshiboyev", "Suxrobbek", 19 # Bir nechta o'zgaruvhilar bir amalda yaratildi va qiyamt bilan bilan taminlandi
+
+
+print (" Familya:",username,"\n","Ism:",name,"\n","yosh",ege) # Yaratilgan o'zgarituvchilarni consolga chop etirib olindi
