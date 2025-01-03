d1865f6d82db3afbb941b2cb75800bb413d6faa2
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2025-01-03 12:22:51 -0500

    Done

---
+++ 09-03.py
@@ -0,0 +1,9 @@
+t_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(t_tuple)
+
+thrid_indx = t_tuple[3]
+eighth_indx = t_tuple[8]
+
+count = thrid_indx + eighth_indx
+
+print ("Indexlar yig'indisi = ",count)
