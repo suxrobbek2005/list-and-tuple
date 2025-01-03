c1e105e5b9d769c98f7fa0c864b99dfa149e41dd
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2025-01-03 09:43:06 -0500

    DOne

---
+++ 06-03.py
@@ -0,0 +1,8 @@
+import sys
+
+# 15 talik list yaraatildi
+l_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+print (l_list)
+print(type(l_list))
+print("List ichidagi elementlar soni = ",sys.getsizeof(l_list))
