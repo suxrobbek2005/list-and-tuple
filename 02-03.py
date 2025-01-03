8c68037c9663d1a05188f73f5aa2ce7a3c5083dd
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2025-01-03 07:08:26 -0500

    Done

---
+++ 02-03.py
@@ -0,0 +1,6 @@
+
+# Listlar ichida tuplelar yaratilib olindi 
+to_list_and_touple = [(12, 3 , 4), (1, 9, 0), (7, 8, 56)]
+
+# Tuplelarni qoidasi buyicha tupleni o'zgartirib bulmaydi
+print (to_list_and_touple)
