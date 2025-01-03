e3e1f08ad2fd88b07a5906b17d7f85b6186a8144
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2025-01-03 08:03:30 -0500

    Done

---
+++ 04-01.py
@@ -0,0 +1,8 @@
+# 10 talik ruyxatlar 
+l_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print (type (l_list))
+print (l_list)
+
+t_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print (type (t_tuple))
+print (t_tuple)
