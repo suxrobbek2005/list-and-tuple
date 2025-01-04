2e40f0a1315ca29f10414f701beaaf2cf9061019
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2025-01-03 23:10:43 -0500

    Done

---
+++ masala01.py
@@ -0,0 +1,8 @@
+l_list = ["salom", 0.4, 3, "najotalim", "samaranda"]
+print("Birinchi list:\n", l_list)
+
+
+string = str(l_list)
+upper_letter = string.upper()
+
+print("O'zgartirilgan list:\n",upper_letter)
