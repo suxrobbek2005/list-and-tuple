b27be5ff529dfff2c3e10b6c4dbd9ca2013d0c64
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2025-01-03 22:58:26 -0500

    Done

---
+++ 11-03.py
@@ -0,0 +1,8 @@
+t_tuple = (1, 2, 3, 4, 3, 6, 7, 8, 3, 10, 11, 3)
+
+count_of_two = t_tuple.count(3)
+
+find_indx = t_tuple.index(11)
+
+print(f"Tupleda '11' - elementining indexi {find_indx}")
+print(f"Tuple ichida '3' marta {count_of_two}, qatnashgan")
