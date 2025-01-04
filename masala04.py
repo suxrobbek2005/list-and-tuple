71a72abcdf240ec408035a8ff4b7fd87185915d9
suxrobbek2005 <yaxshiboyevsuxrobbek8@gmail.com>
2025-01-03 23:48:09 -0500

    Done

---
+++ masala04.py
@@ -0,0 +1,11 @@
+t_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+once_index = t_tuple[1]
+thrid_index = t_tuple[5]
+
+all_index = once_index, thrid_index
+
+string = str (all_index)
+
+print(string)
+print(type(string))
